92617f52e5de51f00ecc0a10ec030beb0147ff78
elmuby <elmuby404@gmail.com>
2024-05-17 23:55:00 +0100

    updating address repository

--- src/main/java/com/onlinebookstore/bookstore/repository/AddressRepository.java
+++ src/main/java/com/onlinebookstore/bookstore/repository/AddressRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.onlinebookstore.bookstore.model.Address;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
-	
+	public void findAddressByNameUser();
 
 }
