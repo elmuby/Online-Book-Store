ccceb89264b1bce535a287904c4a38e1ee1a394d
elmuby <elmuby404@gmail.com>
2024-05-24 22:35:05 +0100

    update

--- src/main/java/com/onlinebookstore/bookstore/config/JwtConstant.java
+++ src/main/java/com/onlinebookstore/bookstore/config/JwtConstant.java
@@ -1,8 +1,17 @@
 package com.onlinebookstore.bookstore.config;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class JwtConstant {
+	@Value("${jwt.secret}")
+	private String secretKey;
+
+	public String getSecretKey() {
+		return secretKey;
+	}
+
 	public static final String SECRET_KEY = "dnamlaaxueuqwertyasdfghjklxcvbnmuiopehehejedbadubdhkabhbdkbdakadbfhdb";
-	
+
 	public static final String JWT_HEADER = "Authorization";
 
 }
--- src/main/java/com/onlinebookstore/bookstore/model/OrderItem.java
+++ src/main/java/com/onlinebookstore/bookstore/model/OrderItem.java
@@ -22,6 +22,46 @@ public class OrderItem {
 	private int quantity;
 	
 	private Double totalPrice;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(Double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
 	
 	
 	
